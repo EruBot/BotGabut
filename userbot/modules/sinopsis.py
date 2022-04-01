4e261d8c49190f9541d249a7912885a2f51355d6
Erruuu <64939964+EruBot@users.noreply.github.com>
2022-04-02 03:52:47 +0700

    add title sinopsis

--- userbot/modules/sinopsis.py
+++ userbot/modules/sinopsis.py
@@ -20,15 +20,17 @@ async def _(event):
         return
     else:
         await event.edit("`please wait..`")
-        msg = "<b>➲ Sinopsis</b>\n"
-        msg += f"<b>From {url}</b>"
-        msg += f"═════════════════"
         neourl = requests.get(url)
         neopage = bs(neourl.content, 'html.parser')
+        altimg = soup.find(itemprop="image")
+        ttl = altimg["alt"]
+        msg = f"<b>➲ Sinopsis <a href='{url}'>{ttl}</a></b>\n"
+        bts = 7*"="
+        msg += f"\n{bts}\n"
         neos = neopage.find("div", class_="contenidotv")
         neop = neos.find(itemprop="description")
         for sino in neop.find_all('p'):
-        	msg += f"<b>{sino}</b>\n"
+            msg += f"<b>{sino}</b>\n"
 
         await event.edit(msg, parse_mode="html")
 
